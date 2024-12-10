aec1f03f83cc5185a64443c5889cc4c451eea92b
palascinochristian <palascinochristian@gmail.com>
2024-12-10 16:03:20 +0100

    selected elements in js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*-----------
+   ELEMENTS
+------------*/
+
+// FORM
+const formUserNameElm = document.getElementById("userName");
+const formKmElm = document.getElementById("userKm");
+const formAgeElm = document.getElementById("userAge")
+// TICKET
+const ticketUserNameElm = document.getElementById("ticketUserName");
+const ticketOfferType = document.getElementById("ticketOfferType");
+const ticketCarriage = document.getElementById("ticketCarriage");
+const ticketCode = document.getElementById("ticketCode");
+const ticketPrice = document.getElementById("ticketPrice");
+
+
